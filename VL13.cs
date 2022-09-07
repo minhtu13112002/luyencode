04aa5e69b098e354fcf64d98adf3920a7caf2944
minhtu13112002 <111958823+minhtu13112002@users.noreply.github.com>
2022-09-07 15:14:55 +0700

    Add files via upload

---
+++ VL13.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace VL13
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            String s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            int tonguoc = 0;
+            if (n < 0) Console.WriteLine("NO");
+            else
+            {
+                for (int i = 1; i < n; i++)
+                    if (n % i == 0) tonguoc = tonguoc + i;
+                if (tonguoc == n) Console.WriteLine("YES");
+                else Console.WriteLine("NO");
+            }        
+                
+                        
+        }
+    }
+}
